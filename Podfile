ce1a991ed6c6de8bdbcaaeb1f7c0ed372f67703f
lxy <lxy@lxydeiMac.local>
2019-03-08 14:44:12 +0800

    RAC学习，pod之后，swift文件报错，添加use_frameworks! 1.use_frameworks! -> dynamic frameworks 方式 -> .framework（动态库，swift的cocopod默认使用）编译时只存储了指向动态库的引用。可以多个程序指向这个库，在运行时才加载，不会使应用体积变大，但是运行时加载会损耗部分性能，并且依赖外部的环境，如果库不存在或者版本不正确则无法运行 2.#use_frameworks! -> static libraries 方式 -> .a（静态库） 在编译时会将库copy一份到目标程序中，编译完成之后，目标程序不依赖外部的库，也可以运行

---
+++ Podfile
@@ -0,0 +1,8 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RAC_Study' do
+   use_frameworks!
+   pod 'ReactiveCocoa'
+
+end
---
+++ RAC_Study/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RAC_Study
+//
+//  Created by lxy on 2019/3/8.
+//  Copyright © 2019年 lxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
